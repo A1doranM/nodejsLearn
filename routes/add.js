414e9e5a13054f9d4788d03cbf4e930c264452f8
Maksym <maksym@digitalmaker.io>
2020-01-27 13:19:30 +0200

    Initial commit

---
+++ routes/add.js
@@ -0,0 +1,24 @@
+const {Router} = require("express");
+const Course = require("../models/course");
+const router = Router();
+
+router.get("/", (req, res) => {
+    res.render("add", {
+        title: "Добавить курс",
+        isAdd: true
+    })
+});
+
+router.post("/", async (req, res) => {
+    const course = new Course({
+        title: req.body.title,
+        price: req.body.price,
+        img: req.body.img
+    });
+
+    await course.save();
+
+    res.redirect("/courses");
+});
+
+module.exports = router;
